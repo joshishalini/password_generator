fd86a197a624bb2c95756afe1afe5616c2b835bf
shalini joshi <shalinijoshi@shalinis-MacBook-Pro.local>
2025-06-03 13:34:20 +0100

    add password generator and rspec

---
+++ lib/password/generator.rb
@@ -0,0 +1,63 @@
+module Password
+  class Generator
+    SPECIAL_CHARS = ["@", "%", "!", "?", "*", "^", "&"]
+    NUMBER_CHARS = ('0'..'9').to_a
+    LOWER_CHARS = ('a'..'z').to_a
+    UPPER_CHARS = ('A'..'Z').to_a
+
+    attr_reader :length, :uppercase, :lowercase, :number, :special
+
+    def initialize(length:, uppercase: true, lowercase: true, number: 2, special: 1)
+		  @length = length
+		  @uppercase = uppercase
+		  @lowercase = lowercase
+		  @number = number
+		  @special = special
+		  validate_options
+		end
+
+    def generate
+      password = []
+      password += special_chars
+      password += number_chars
+      password += required_letters
+
+      remaining = length - password.size
+      password += Array.new(remaining) { available_letters.sample }
+
+      password.shuffle.join
+    end
+
+    private
+
+    def validate_options
+      raise ArgumentError, 'Length must be positive' unless length.positive?
+      raise ArgumentError, 'At least one of uppercase or lowercase must be true' unless uppercase || lowercase
+      raise ArgumentError, 'Sum of number and special characters exceeds length' if (number + special) > length
+    end
+
+    def special_chars
+      Array.new(special) { SPECIAL_CHARS.sample }
+    end
+
+    def number_chars
+      Array.new(number) { NUMBER_CHARS.sample }
+    end
+
+    def required_letters
+      letters = []
+      letters << UPPER_CHARS.sample if uppercase
+      letters << LOWER_CHARS.sample if lowercase
+      letters
+    end
+
+    def available_letters
+      chars = []
+      chars += UPPER_CHARS if uppercase
+      chars += LOWER_CHARS if lowercase
+      chars
+    end
+  end
+end
+
+# puts Password::Generator.new(length: 9, uppercase: false, lowercase: true, number: 2, special: 1).generate
---
+++ spec/password/generator_spec.rb
@@ -0,0 +1,59 @@
+require 'rspec'
+
+require_relative '../../lib/password/generator'
+
+RSpec.describe Password::Generator do
+  context 'valid password generation' do
+    it 'generate with correct length' do
+      generator = described_class.new(length: 12, number: 2, special: 2)
+      password = generator.generate
+      expect(password.length).to eq(12)
+    end
+
+    it 'generate with minimum one lowercase char' do
+      generator = described_class.new(length: 10, uppercase: false, lowercase: true, number: 2, special: 1)
+      password = generator.generate
+      expect(password).to match(/[a-z]/)
+    end
+
+    it 'generate with minimum one uppercase char' do
+      generator = described_class.new(length: 5, uppercase: true, lowercase: false, number: 3, special: 1)
+      password = generator.generate
+      expect(password).to match(/[A-Z]/)
+    end
+
+    it 'generate with expect 2 number' do
+      generator = described_class.new(length: 5, uppercase: false, lowercase: true, number: 3, special: 1)
+      password = generator.generate
+      digit_count = password.scan(/\d/).count
+      expect(digit_count).to eq(3)
+    end
+
+    it 'includes the correct number of special characters' do
+      generator = described_class.new(length: 10, number: 2, special: 2)
+      password = generator.generate
+      special_count = password.count(Password::Generator::SPECIAL_CHARS.join)
+      expect(special_count).to eq(2)
+    end
+  end
+
+  context 'error handling' do
+    it 'raises error for non-positive length' do
+      expect {
+        described_class.new(length: 0, number: 1, special: 1)
+      }.to raise_error(ArgumentError, /Length must be positive/)
+    end
+
+    it 'raises error when number + special exceeds length' do
+      expect {
+        described_class.new(length: 4, number: 3, special: 2)
+      }.to raise_error(ArgumentError, /Sum of number and special/)
+    end
+
+    it 'raises error when both uppercase and lowercase are false' do
+      expect {
+        described_class.new(length: 8, uppercase: false, lowercase: false, number: 1, special: 1)
+      }.to raise_error(ArgumentError, /At least one/)
+    end
+  end
+end
